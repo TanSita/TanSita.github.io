a0007761a555459ff189590d6892e6dd897f3a69
TS <nightquiet813@gmail.com>
2017-12-04 16:00:00 +0800

    YO

--- Menu/js/output2.js
+++ Menu/js/output2.js
@@ -700,48 +700,6 @@ function makeListTextbox(num,new_or_edit,huding,ts)
     return myhtml;
 }
 
-function makeList(num , listText , huding , ts)
-{
-    var listID = "list" + num;
-    var categoryID = "category" + num;
-
-    var myhtml = 
-    '<li id="' + listID + '">' +
-        '<a href="#' + categoryID + '">' + listText +
-        '</a>' + 
-        makeEditDeleteButton(num,huding,ts) +
-    '</li>';
-
-    return myhtml;
-}
-
-function makeCategory(num , listText)
-{
-    var categoryID = "category" + num;
-    var categoryTitleID = "categoryTitle" + num;
-
-    var myhtml = 
-    '<div class="panel panel-' + mypanel_colors[num%mypanel_colorsLen] + '">' +
-        '<div class="panel-heading">' +
-             '<div class="row">' + 
-                '<div class="alignleft">' +
-                    '<strong class="panel-title" id="' + categoryTitleID + '">' + listText + '</strong>' +
-                '</div>' +
-                '<div class="alignright">' +
-                    '<button class="btn btn-success " onclick="addAlert(' + num + ')">' + '新增</button>' +
-                '</div>' +
-            '</div>' +
-        '</div>' +
-
-        '<div id ="' + categoryID + '" class="panel-body color-' + mypanel_colors[num%mypanel_colorsLen] + '">' +
-
-        '</div>' +
-    '</div>';
-
-
-    return myhtml;
-}
-
 function editListOP(num , listText , flag , flagID , editListedit , newListadd)
 {
     var mycategoryID = "category" + num;
@@ -882,29 +840,6 @@ function makeOKDeleteButton(num,new_or_edit,huding,ts)
 
     return myhtml;
 }
-
-function makeEditDeleteButton(num , huding , ts)
-{
-    var myhtml = 
-    '<button class="btn nothing"></button>' + 
-    '<button class="btn btn-danger pull-right"' + 
-    'onclick="DeleteItem(' + num + ',' + huding + ',' + ts + ');" >' + 
-    '   <span class="glyphicon glyphicon-trash"></span>' + 
-    '</button>' + 
-    '<button class="btn btn-info pull-right"' + 
-    'onclick="EditItem(' + num + ',' + huding + ',' + ts + ');" >' + 
-    '   <span class="glyphicon glyphicon-pencil"></span>' + 
-    '</button>';
-
-    return myhtml;
-}
-
-
-
-
-
-
-
 
 
 
--- TryJson/js/MenuSetting.js
+++ TryJson/js/MenuSetting.js
@@ -365,38 +365,6 @@ function makeListDemo(num,listText)
 	return myhtml;
 }
 
-
-function makeList(num , listText , huding , ts)
-{
-	var listID = "list" + num;
-	var categoryID = "category" + num;
-
-	var myhtml = 
-	'<li id="' + listID + '">' +
-		'<a href="#' + categoryID + '">' + listText +
-		'</a>' + 
-		makeEditDeleteButton(num,huding,ts) +
-	'</li>';
-
-	return myhtml;
-}
-
-function makeEditDeleteButton(num , huding , ts)
-{
-    var myhtml = 
-    '<button class="btn nothing"></button>' + 
-    '<button class="btn btn-danger pull-right"' + 
-    'onclick="DeleteItem(' + num + ',' + huding + ',' + ts + ');" >' + 
-    '   <span class="glyphicon glyphicon-trash"></span>' + 
-    '</button>' + 
-    '<button class="btn btn-info pull-right"' + 
-    'onclick="EditItem(' + num + ',' + huding + ',' + ts + ');" >' + 
-    '   <span class="glyphicon glyphicon-pencil"></span>' + 
-    '</button>';
-
-    return myhtml;
-}
-
 function makeCategoryDemo(num,listText)
 {
 	var categoryID = "category" + num;
@@ -517,33 +485,6 @@ function maketop10(hudingID , tsID , imageID , name , price , src , intro , top1
     '</a>';
 
     return myhtml;
-}
-
-function makeCategory(num , listText)
-{
-	var categoryID = "category" + num;
-	var categoryTitleID = "categoryTitle" + num;
-
-	var myhtml = 
-	'<div class="panel panel-' + mypanel_colors[num%mypanel_colorsLen] + '">' +
-	    '<div class="panel-heading">' +
-    		 '<div class="row">' + 
-    		 	'<div class="alignleft">' +
-    		 		'<strong class="panel-title" id="' + categoryTitleID + '">' + listText + '</strong>' +
-		 		'</div>' +
-		 		'<div class="alignright">' +
-            		'<button class="btn btn-success " onclick="addAlert(' + num + ');">' + '新增</button>' +
-        		'</div>' +
-        	'</div>' +
-    	'</div>' +
-
-    	'<div id ="' + categoryID + '" class="panel-body color-' + mypanel_colors[num%mypanel_colorsLen] + '">' +
-
-		'</div>' +
-	'</div>';
-
-
-	return myhtml;
 }
 
 
