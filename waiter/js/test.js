3c8a4ba22b4a4f0ef9018b8adfba1a664e6cb97c
TS <nightquiet813@gmail.com>
2017-12-22 03:24:04 +0800

    final settings

--- waiter/js/test.js
+++ waiter/js/test.js
@@ -82,5 +82,9 @@ delOrder(5,5);
 console.log(forhuding);
 // console.log(JSON.stringify(forhuding));
 
-window.onunload = window.onbeforeunload = 
-function () { return "正在點餐中，確定要離開?" };
+$(window).on('beforeunload',function(){return 'Your own message goes here...';});
+$(window).on('unload',function(){return'Your own message goes here...';});
+
+
+// window.onunload = window.onbeforeunload = 
+// function () { return "正在點餐中，確定要離開?" };
